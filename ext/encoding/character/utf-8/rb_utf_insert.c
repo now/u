f7d589730b096a9af99f7e6ec6d52f1be71f975a
Nikolai Weibull <now@bitwi.se>
2011-03-21 15:13:10 +0100

    Rewrite stale specifications as expectations

--- ext/encoding/character/utf-8/rb_utf_insert.c
+++ ext/encoding/character/utf-8/rb_utf_insert.c
@@ -6,19 +6,24 @@
 
 #include "rb_includes.h"
 
-/* TODO: Update to use new offset-calculating functions. */
 VALUE
 rb_utf_insert(UNUSED(VALUE self), VALUE str, VALUE index, VALUE other)
 {
         long offset = NUM2LONG(index);
-
         StringValue(str);
 
         long n_chars = utf_length_n(RSTRING(str)->ptr, RSTRING(str)->len);
-
+        /*
+        char *begin, *end;
+        if (offset < 0)
+                offset++;
+        rb_utf_begin_from_offset_validated(str, offset, &begin, &end);
+        puts(begin);
+        rb_str_update(str, begin - RSTRING(str)->ptr, 0, other);
+        */
         if (abs(offset) > n_chars) {
                 if (offset < 0)
-                        offset -= n_chars;
+                        offset += n_chars;
                 rb_raise(rb_eIndexError, "index %ld out of string", offset);
         }
 
--- test/unit/u/string.rb
+++ test/unit/u/string.rb
@@ -29,5 +29,63 @@ Expectations do
   expect 3 do 'hello'.u.count('world') end
 
   expect ''.u.delete('whatever').to.be.empty?
-  expect 'hëllo' do 'hëllö'.u.delete('ö') end
+  expect 'hëll' do 'hëllö'.u.delete('ö') end
+
+  expect 0 do
+    i = 0
+    ''.u.each_char{ i += 1 }
+    i
+  end
+
+  expect %w[h ë l l ö] do
+    [].tap{ |cs| 'hëllö'.u.each_char{ |c| cs << c } }
+  end
+
+  expect 0 do ''.u.index('') end
+  expect nil do ''.u.index('', 1) end
+  expect nil do ''.u.index('', -1) end
+  expect 0 do 'hëllö'.u.index('hë') end
+  expect 3 do 'hëllö'.u.index('lö') end
+  expect 3 do 'hëllö'.u.index('lö', 3) end
+
+  expect '' do ''.u.insert(0, '') end
+  expect IndexError do ''.u.insert(1, '') end
+  expect 'äbc' do ''.u.insert(0, 'äbc') end
+  expect 'hëöll' do 'hëö'.u.insert(3, 'll') end
+  expect 'hëöll' do 'hëö'.u.insert(-1, 'll') end
+  expect 'hëllö' do 'hëö'.u.insert(2, 'll') end
+  expect 'hëllö' do 'hëö'.u.insert(-2, 'll') end
+  expect 'llhëö' do 'hëö'.u.insert(0, 'll') end
+  #expect 'llhëö' do 'hëö'.u.insert(-4, 'll') end
+
+  expect 0 do ''.u.length end
+  expect 5 do 'hëllö'.u.length end
+  expect 11 do "hëllö\0agäin".u.length end
+  # TODO: Why not throw an error here?
+  expect 11 do "hëllö\0agäin\303".u.length end
+
+  expect 0 do ''.u.rindex('') end
+  expect 0 do 'hëllö'.u.rindex('hë') end
+  expect 3 do 'hëllö'.u.rindex('lö') end
+  expect 3 do 'hëllö'.u.rindex('lö', 3) end
+  expect 5 do 'hëllölö'.u.rindex('lö') end
+  expect 5 do 'hëllölö'.u.rindex('lö', 5) end
+  expect 3 do 'hëllölö'.u.rindex('lö', 4) end
+
+  expect ''.u.squeeze.to.be.empty?
+  expect 'hëlö' do 'hëllö'.u.squeeze end
+
+  expect 0 do ''.u.to_i end
+  expect 0 do ''.u.to_i(0) end
+  expect ArgumentError do ''.u.to_i(-1) end
+  expect ArgumentError do ''.u.to_i(1) end
+  expect ArgumentError do ''.u.to_i(37) end
+  expect 1 do '1'.u.to_i end
+  expect 1 do '1'.u.to_i(2) end
+  expect 1 do '١'.u.to_i end
+
+  expect ''.u.tr('abc', 'def').to.be.empty?
+  expect 'abcde' do 'äbcdë'.u.tr('äë', 'ae') end
+  expect 'ëëëëë' do 'äbcdë'.u.tr('a-zäë', 'ë') end
+  expect 'ëëëëë' do 'aaaaa'.u.tr('a', 'ä-ë') end
 end
