43c5aebea8b12f3e28b464cf37d7d0a46570bcc2
Nikolai Weibull <now@bitwi.se>
2011-03-21 14:09:19 +0100

    Further fixes to extconf.rb

--- ext/encoding/character/utf-8/extconf.rb
+++ ext/encoding/character/utf-8/extconf.rb
@@ -12,14 +12,10 @@ try_compiler_option '-fno-common'
 try_compiler_option '-Wall'
 try_compiler_option '-Waggregate-return'
 try_compiler_option '-Wcast-align'
-# NOTE: Generates way too many false positives.
-# try_compiler_option '-Wconversion'
 try_compiler_option '-Wextra'
 try_compiler_option '-Wformat=2'
 try_compiler_option '-Winit-self'
 try_compiler_option '-Winline'
-# NOTE: We need bigger ones for TR_TABLE_SIZE.
-#try_compiler_option '-Wlarger-than-65500'
 try_compiler_option '-Wmissing-declarations'
 try_compiler_option '-Wmissing-format-attribute'
 try_compiler_option '-Wmissing-include-dirs'
@@ -30,29 +26,22 @@ try_compiler_option '-Wold-style-definition'
 try_compiler_option '-Wpacked'
 try_compiler_option '-Wp,-D_FORTIFY_SOURCE=2'
 try_compiler_option '-Wpointer-arith'
-# NOTE: ruby.h and intern.h have too many of these.
-# try_compiler_option '-Wredundant-decls'
-# NOTE: Complains about index, for example.
-# try_compiler_option '-Wshadow'
 try_compiler_option '-Wsign-compare'
 try_compiler_option '-Wstrict-aliasing=2'
-# NOTE: ruby.h and intern.h are messed up and for some reason GCC wants to
-# complain about them as well.  System headers?
 try_compiler_option '-Wswitch-default'
 try_compiler_option '-Wswitch-enum'
 try_compiler_option '-Wundef'
-# NOTE: Incredible amounts of false positives.
-#try_compiler_option '-Wunreachable-code'
 try_compiler_option '-Wunsafe-loop-optimizations'
 try_compiler_option '-Wwrite-strings'
 
 checking_for 'GNUC visibility attribute' do
   $defs.push('-DHAVE_GNUC_VISIBILITY') if try_compile <<EOC, '-Werror'
+void f_hidden(void);
 void __attribute__((visibility("hidden")))
 f_hidden(void)
 {
 }
-int main(int argc, char **argv)
+int main(void)
 {
   f_hidden();
   return 0;
