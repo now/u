2f5b6b4ba5348eddbfa3b98fe70cd77a274f4c97
Nikolai Weibull <now@bitwi.se>
2011-05-28 10:49:06 +0200

    Fix u_is_ascii_only

--- ext/u/u_is_ascii_only.c
+++ ext/u/u_is_ascii_only.c
@@ -9,7 +9,7 @@ static bool
 u_is_ascii_only_impl(const char *string, size_t length, bool use_length)
 {
         for (const char *p = string; P_WITHIN_STR(p, string, length, use_length); p++)
-                if (*p & 0x80)
+                if (*(unsigned char *)p > 127)
                         return false;
 
         return true;
