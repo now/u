84d3342d8ed5cac815020339621b72859954760c
Nikolai Weibull <now@bitwi.se>
2013-04-01 22:11:01 +0200

    Fix a -Wcast-align warning
    I don’t know if this is OK, but I think it should work, considering that
    bsearch() works similarly.

--- ext/u/private.c
+++ ext/u/private.c
@@ -16,7 +16,7 @@
 bool
 binary_search_unicode_table(const void *table, size_t n, size_t sizeof_entry, size_t sizeof_char, uint32_t c, size_t *index)
 {
-#define ENTRY(index) (*(uint32_t *)((const char *)table + ((index) * sizeof_entry)) & char_mask)
+#define ENTRY(index) (*(uint32_t *)(void *)((const char *)table + ((index) * sizeof_entry)) & char_mask)
 
 	size_t begin = 0;
         size_t end = n - 1;
