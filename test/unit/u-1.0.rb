0d45c7a8f5456ac6bfc9b0da4c0bfdcbed5dcce1
Nikolai Weibull <now@bitwi.se>
2013-04-03 13:54:32 +0200

    Add tests for String#u

--- test/unit/u-1.0.rb
+++ test/unit/u-1.0.rb
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 Expectations do
+  expect U::String do ''.u end
+  expect U::String.new('abc') do 'abc'.u end
   expect result.tainted? do ''.taint.u end
   expect result.untrusted? do ''.untrust.u end
 end
