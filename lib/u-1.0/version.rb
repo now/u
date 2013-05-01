e18ca9e22509bb5a020a4e0ecb47bc62bab2422e
Nikolai Weibull <now@bitwi.se>
2013-05-01 11:57:25 +0200

    Add Lookout dependency

--- lib/u-1.0/version.rb
+++ lib/u-1.0/version.rb
@@ -8,6 +8,7 @@ module U
       super + Inventory::Dependencies.new{
         development 'inventory-rake', 1, 5, 0
         development 'inventory-rake-tasks-yard', 1, 3, 0
+        development 'lookout', 3, 0, 0
         development 'lookout-rake', 3, 0, 0
         development 'yard', 0, 8, 0
         development 'yard-heuristics', 1, 1, 0
